796776266e580939c669169e045094ccbe2bfdc0
Andrea <estefania230@hotmail.com>
2019-02-21 21:28:28 -0600

    First commit

---
+++ src/main/java/com/example/PEAEE1/Peaee1Application.java
@@ -0,0 +1,13 @@
+package com.example.PEAEE1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Peaee1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Peaee1Application.class, args);
+	}
+
+}
